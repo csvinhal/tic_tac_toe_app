16f0f7f9973c6a371062800614ef0e6db247190f
cristiano_vinhal <cristiano.vinhal@br.experian.com>
2023-05-12 15:26:06 -0300

    add icon literal strings

---
+++ lib/module/app_icons_assets.dart
@@ -0,0 +1,23 @@
+class AppIconsAssets {
+  AppIconsAssets._();
+
+  static const backArrow = '$_path/back_arrow.png';
+  static const backArrowDark = '$_path/back_arrow_dark.png';
+
+  static const date = '$_path/date.png';
+  static const dateDark = '$_path/date_dark.png';
+
+  static const facebook = '$_path/facebook.png';
+  static const facebookDark = '$_path/facebook_dark.png';
+
+  static const google = '$_path/google.png';
+  static const googleDark = '$_path/google_dark.png';
+
+  static const retry = '$_path/retry.png';
+  static const retryDark = '$_path/retry_dark.png';
+
+  static const search = '$_path/search.png';
+  static const searchDark = '$_path/search_dark.png';
+
+  static const _path = 'assets/icons';
+}

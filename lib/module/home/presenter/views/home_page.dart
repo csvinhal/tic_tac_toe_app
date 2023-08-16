bd20e92c3b7fae3099791582435a1231601b8333
Cristiano Vinhal <cristiano.vinhal10@gmail.com>
2023-08-15 22:54:11 -0300

    feat: remove unused widget

--- lib/module/home/presenter/views/home_page.dart
+++ lib/module/home/presenter/views/home_page.dart
@@ -22,117 +22,113 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Stack(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 40.h),
-                const Center(
-                  child: AppTypography(
-                    'Welcome',
-                    type: AppTypographyType.subtitle1,
-                  ),
-                ),
-                const Center(
-                  child: AppTypography(
-                    'Truman Winterbottom',
-                    type: AppTypographyType.header1,
-                  ),
-                ),
-                SizedBox(height: 16.h),
-                SizedBox(
-                  height: 94.h,
-                  child: Center(
-                    child: Score(
-                      children: [
-                        ScoreItem(
-                          label: AppLocalizations.of(context)
-                              .homeHomePageScoreWins,
-                          value: '0',
-                        ),
-                        ScoreItem(
-                          label: AppLocalizations.of(context)
-                              .homeHomePageScoreLosses,
-                          value: '0',
-                        ),
-                        ScoreItem(
-                          label: AppLocalizations.of(context)
-                              .homeHomePageScoreDraws,
-                          value: '0',
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16.h),
-                AppTypography(
-                  AppLocalizations.of(context).homeHomePageGameHistoryTitle,
-                  type: AppTypographyType.subtitle1,
-                ),
-                SizedBox(height: 12.h),
-                BlocBuilder<ThemeCubit, ThemeMode>(
-                  bloc: _themeCubit,
-                  builder: (context, state) {
-                    return GameHistory(
-                      games: [
-                        Game(
-                          opponentsName: 'Will Smith',
-                          date: DateTime.now(),
-                          status: GameStatusType.won,
-                        ),
-                        Game(
-                          opponentsName: 'Will Smith',
-                          date: DateTime.now(),
-                          status: GameStatusType.lost,
-                        ),
-                        Game(
-                          opponentsName: 'Will Smith',
-                          date: DateTime.now(),
-                          status: GameStatusType.won,
-                        ),
-                        Game(
-                          opponentsName: 'Will Smith',
-                          date: DateTime.now(),
-                          status: GameStatusType.lost,
-                        ),
-                        Game(
-                          opponentsName: 'Will Smith',
-                          date: DateTime.now(),
-                          status: GameStatusType.draw,
-                        ),
-                      ],
-                      isDarkMode: _themeCubit.isDarkMode,
-                    );
-                  },
-                ),
-                SizedBox(height: 24.h),
-                AppTypography(
-                  AppLocalizations.of(context).homeHomePageScoreboardTitle,
-                  type: AppTypographyType.subtitle1,
-                ),
-                SizedBox(height: 12.h),
-                BlocBuilder<ThemeCubit, ThemeMode>(
-                  bloc: _themeCubit,
-                  builder: (context, state) {
-                    return Scoreboard(
-                      scores: const [
-                        ScoreboardItem(nickname: 'Will Smith', value: 250),
-                        ScoreboardItem(nickname: 'Will Smith', value: 230),
-                        ScoreboardItem(nickname: 'Will Smith', value: 210),
-                        ScoreboardItem(nickname: 'Will Smith', value: 190),
-                        ScoreboardItem(nickname: 'Will Smith', value: 170),
-                      ],
-                      isDarkMode: _themeCubit.isDarkMode,
-                    );
-                  },
-                ),
-              ],
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40.h),
+            const Center(
+              child: AppTypography(
+                'Welcome',
+                type: AppTypographyType.subtitle1,
+              ),
             ),
-          ),
-        ],
+            const Center(
+              child: AppTypography(
+                'Truman Winterbottom',
+                type: AppTypographyType.header1,
+              ),
+            ),
+            SizedBox(height: 16.h),
+            SizedBox(
+              height: 94.h,
+              child: Center(
+                child: Score(
+                  children: [
+                    ScoreItem(
+                      label: AppLocalizations.of(context)
+                          .homeHomePageScoreWins,
+                      value: '0',
+                    ),
+                    ScoreItem(
+                      label: AppLocalizations.of(context)
+                          .homeHomePageScoreLosses,
+                      value: '0',
+                    ),
+                    ScoreItem(
+                      label: AppLocalizations.of(context)
+                          .homeHomePageScoreDraws,
+                      value: '0',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16.h),
+            AppTypography(
+              AppLocalizations.of(context).homeHomePageGameHistoryTitle,
+              type: AppTypographyType.subtitle1,
+            ),
+            SizedBox(height: 12.h),
+            BlocBuilder<ThemeCubit, ThemeMode>(
+              bloc: _themeCubit,
+              builder: (context, state) {
+                return GameHistory(
+                  games: [
+                    Game(
+                      opponentsName: 'Will Smith',
+                      date: DateTime.now(),
+                      status: GameStatusType.won,
+                    ),
+                    Game(
+                      opponentsName: 'Will Smith',
+                      date: DateTime.now(),
+                      status: GameStatusType.lost,
+                    ),
+                    Game(
+                      opponentsName: 'Will Smith',
+                      date: DateTime.now(),
+                      status: GameStatusType.won,
+                    ),
+                    Game(
+                      opponentsName: 'Will Smith',
+                      date: DateTime.now(),
+                      status: GameStatusType.lost,
+                    ),
+                    Game(
+                      opponentsName: 'Will Smith',
+                      date: DateTime.now(),
+                      status: GameStatusType.draw,
+                    ),
+                  ],
+                  isDarkMode: _themeCubit.isDarkMode,
+                );
+              },
+            ),
+            SizedBox(height: 24.h),
+            AppTypography(
+              AppLocalizations.of(context).homeHomePageScoreboardTitle,
+              type: AppTypographyType.subtitle1,
+            ),
+            SizedBox(height: 12.h),
+            BlocBuilder<ThemeCubit, ThemeMode>(
+              bloc: _themeCubit,
+              builder: (context, state) {
+                return Scoreboard(
+                  scores: const [
+                    ScoreboardItem(nickname: 'Will Smith', value: 250),
+                    ScoreboardItem(nickname: 'Will Smith', value: 230),
+                    ScoreboardItem(nickname: 'Will Smith', value: 210),
+                    ScoreboardItem(nickname: 'Will Smith', value: 190),
+                    ScoreboardItem(nickname: 'Will Smith', value: 170),
+                  ],
+                  isDarkMode: _themeCubit.isDarkMode,
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
